966658bb664257f3efb4d9f405a947746cbe64ce
sumeyahajji <abdinasirsumeya@gmail.com>
2023-09-04 20:24:28 +0300

    completed variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace VariablesExercise
+﻿using System;
+
+namespace VariablesExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Ralph";
+            int dogAge = 10;
+            char arrayNumber = 'a';
+            bool isName = true;
+            double doubleNum = 1.33;
+            decimal decNum = 13.990m;
+
+            Console.WriteLine($"its {isName} that {dogName}  is {arrayNumber} {dogAge} year old  dog");
+            Console.WriteLine($"My dogs name is {dogName}, He is {dogAge} years old" );
+            Console.WriteLine($"double number is {doubleNum} while decimal number is {decNum}");
+            
         }
     }
 }
